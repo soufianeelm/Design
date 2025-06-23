313cb9d3c2f002d0a662d49d7499b146c37b8af7
Soufiane El Mouahid <s.elmouahid@hotmail.com>
2025-06-23 15:05:53 +0200

    Update error_popup.py

--- error_popup.py
+++ error_popup.py
@@ -9,6 +9,15 @@ print(" " * 10 + "A RESTART ATTEMPT WILL BE MADE AUTOMATICALLY 5 SECONDS AFTER S
 print()
 print(" " * 10 + f"CHECK THE LOGS FOR MORE DETAILS.")
 print()
+print("\n" + "="*100)
+print(" " * 20 + "!!!   KRITISCHE SYSTEMMITTEILUNG   !!!")
+print("="*100)
+print()
+print(" " * 10 + "DAS SYSTEM WIRD IN 10 SEKUNDEN HERUNTERGEFAHREN WEGEN EINES FEHLERS.")
+print(" " * 10 + "EIN NEUSTART WIRD AUTOMATISCH 5 SEKUNDEN NACH DEM HERUNTERFAHREN VERSUCHT.")
+print()
+print(" " * 10 + "WEITERE DETAILS FINDEN SIE IN DEN LOGDATEIEN.")
+print()
 print("="*100 + "\n")
     
 time.sleep(10)  # Wait before killing the process
